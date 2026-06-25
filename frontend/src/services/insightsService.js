20c59bcb9336feec007a0b1572c41c486ccc1a04
Rizwan Bashir <bashirrizwan54@gmail.com>
2026-06-25 23:00:39 +0530

    Delete frontend directory

--- frontend/src/services/insightsService.js
+++
@@ -1,9 +0,0 @@
-import api from './api'
-
-export const insightsService = {
-  generateInsights: (data) => api.post('/insights', data),
-
-  getInsightsHistory: () => api.get('/insights/history'),
-
-  chat: (message, context) => api.post('/insights/chat', { message, context }),
-}
--- frontend/src/services/segmentationService.js
+++
@@ -1,8 +0,0 @@
-import api from './api'
-
-export const segmentationService = {
-  runSegmentation: (datasetId, params) => 
-    api.post('/segmentation', { datasetId, ...params }),
-
-  getSegmentationHistory: () => api.get('/segmentation/history'),
-}
