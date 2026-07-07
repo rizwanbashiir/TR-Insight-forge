42831dc911061aeab58cec09e9ec3035ebec6e3c
rizwanbashiir <bashirrizwan54@email.com>
2026-07-07 16:14:02 +0530

    lean requirements

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -16,6 +16,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str = "postgresql://postgres@localhost:5432/analytics_db"
     
     # JWT
+    PORT: int = 8000
     SECRET_KEY: str = ""
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24  # 24 hours
